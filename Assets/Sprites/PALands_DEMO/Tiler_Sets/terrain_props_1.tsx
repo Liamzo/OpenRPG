58f44713ec5ce58950a7988fc4e6f4c9fed42038
Liam Meagher <zliamzo@gmail.com>
2023-10-29 17:11:17 +0000

    decoration

---
+++ Assets/Sprites/PALands_DEMO/Tiler_Sets/terrain_props_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="terrain_props_1" tilewidth="16" tileheight="16" tilecount="1400" columns="28">
+ <image source="../16x16/_PNG/terrain_props_1.png" width="448" height="800"/>
+</tileset>
