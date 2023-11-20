57e2f31b445d4bee390b4064ddb1bcd849442921
Liam Meagher <zliamzo@gmail.com>
2023-11-20 16:16:21 +0000

    a

--- Assets/Scripts/Weapons/Misc/WeaponIdleFollow.cs
+++ Assets/Scripts/Weapons/Misc/WeaponIdleFollow.cs
@@ -40,6 +40,7 @@ public class WeaponIdleFollow : MonoBehaviour
 
     protected void IdleFollow() {
         if (doFollow == false) { return; }
+        
 
         Vector3 movePos = Vector3.SmoothDamp(prevPosition, transform.TransformPoint(Vector3.zero), ref distVelocity, smoothTimeDist);
 
