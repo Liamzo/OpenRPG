6c6460b6a78a035a6835cac42c3a909c238cfc31
Liamzo <zliamzo@gmail.com>
2024-06-02 21:40:17 +0100

    a

--- Assets/Scripts/Objects/HandSpot.cs
+++ Assets/Scripts/Objects/HandSpot.cs
@@ -52,11 +52,5 @@ public class HandSpot : MonoBehaviour
 
         transform.localPosition = spotPos * offset;
         transform.up = diff * -1;
-
-        Debug.Log("diff: " + diff);
-        Debug.Log(target);
-        Debug.Log(owner.position);
-        Debug.Log(spotPos);
-
     }
 }
