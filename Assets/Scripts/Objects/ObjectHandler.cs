e89530cece8f1797a9a6f33c0ad7bfbfc19de291
Liamzo <zliamzo@gmail.com>
2024-07-14 22:52:41 +0100

    a

--- Assets/Scripts/Objects/ObjectHandler.cs
+++ Assets/Scripts/Objects/ObjectHandler.cs
@@ -14,8 +14,8 @@ public class ObjectHandler : MonoBehaviour
     static int idIncrementor = 0;
     public int objectHandlerId; // Unique per instance ideally
 
-    public SpriteRenderer spriteRenderer{get; private set;}
-    public Rigidbody2D rigidBody{get; private set;}
+    public SpriteRenderer spriteRenderer {get; private set;}
+    public Rigidbody2D rigidBody {get; private set;}
     public Collider2D Collider {get; private set;}
     public AudioSource audioSource {get; private set;}
 
