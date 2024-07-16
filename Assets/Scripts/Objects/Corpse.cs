c8c58be0d9d2cfdf3db313227c20d081301da9a0
Liamzo <zliamzo@gmail.com>
2024-07-16 20:07:32 +0100

    corpse bug fix

--- Assets/Scripts/Objects/Corpse.cs
+++ Assets/Scripts/Objects/Corpse.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class Corpse : MonoBehaviour
 {
+    public BaseStats baseStats;
     public SpriteRenderer spriteRenderer;
     public InventoryHandler inventoryHandler;
 
 
     public void SetVars(SpriteRenderer renderer, List<ItemHandler> items) {
-        spriteRenderer.sprite = renderer.sprite;
-        spriteRenderer.flipX = renderer.flipX;
-        inventoryHandler.inventory = items;
-
         ObjectHandler owner = GetComponent<ObjectHandler>();
+        owner.CreateBaseObject(baseStats);
 
+        inventoryHandler.inventory = items;
         foreach(ItemHandler item in inventoryHandler.inventory) {
             item.owner = owner;
             item.transform.parent = transform;
         }
+
+        spriteRenderer.sprite = renderer.sprite;
+        spriteRenderer.flipX = renderer.flipX;
     }
 }
